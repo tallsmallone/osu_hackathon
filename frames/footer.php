075cedde9cbb1315a3d787adb468b37b80375b05
Nick Flint <nick.r.flint@gmail.com>
2015-11-14 14:02:02 -0500

    Updated google analytics

--- frames/footer.php
+++ frames/footer.php
@@ -3,5 +3,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <?php
+      include_once("http://osudining.warlockgaming.com/frames/google.php");
+    ?>
 <body>
 </html>
