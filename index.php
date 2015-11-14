2ed34badcb8c54bfdc032356a658b15ee3be16ee
Nick Flint <nick.r.flint@gmail.com>
2015-11-14 17:54:29 -0500

    Fixed errors

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 	require_once("frames/head.php"); 
 ?>
 	<div style="width:100%; height=500px; margin:auto">
-		<img src="/img/eduDine.png" class="logo" style="margin-left:auto; margin-right:auto; display:block;">
-		<div class="input-group input-group-lg"  style="left:25%; width:50%;">
+		<img src="img/eduDine.png" class="logo">
+		<div class="input-group input-group-lg" id="search">
 	   		<input type="text" class="form-control" placeholder="Where to Eat" aria-describedby="sizing-addon1">
-	   		<span class="input-group-btn">
-        		<button class="btn btn-default" type="button">Search</button>
-      		</span>
 	   	</div>
 	</div>
 <?php
