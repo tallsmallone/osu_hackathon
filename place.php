d68645e76f02b99e5c62d57e745273f4b38e084a
Stephen Wu <wujstep@gmail.com>
2015-11-14 15:55:23 -0500

    temporary place page & README

---
+++ place.php
@@ -0,0 +1,11 @@
+<?php
+	require_once("frames/head.php"); 
+?>
+	<div>
+		<div class="input-group input-group-lg"  style="top:250px; left:25%; width:50%;">
+	   		<input type="text" class="form-control" placeholder="Where to Eat" aria-describedby="sizing-addon1">
+	   	</div>
+	</div>
+<?php
+  	require_once("frames/footer.php");
+?>
