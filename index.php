a45ded7f66e5501c5f14c58a95295df8ba32c155
Nick Flint <nick.r.flint@gmail.com>
2015-11-14 12:30:38 -0500

    Added index page with frames initiated

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once("frames/head.php"); 
+?>
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+  <?php
+  	require_once("frames/footer.php");
+  ?>
