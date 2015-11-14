bfa7e0c9ab36d08ba8a3e4b20f7ce5276817bd36
Stephen Wu <wujstep@gmail.com>
2015-11-14 16:47:58 -0500

    place template

--- place.php
+++ place.php
@@ -5,6 +5,18 @@
 		<div class="input-group input-group-lg"  style="top:250px; left:25%; width:50%;">
 	   		<input type="text" class="form-control" placeholder="Where to Eat" aria-describedby="sizing-addon1">
 	   	</div>
+		<h3>Place name</h3>
+		Open/Closed<br>
+		Accepts: Swipe, Dining Dollars, BuckID<br>
+		Type: Coffee Shop, etc.<br>
+		Address: <br>
+		Hours: <br>
+		Today: 11:00 a.m. - 11:00 p.m.<br>
+		Saturday: <br>
+		Sunday: etc.<br>
+		Tags: [gluten-free], [vegan], etc.
+		<br>
+		
 	</div>
 <?php
   	require_once("frames/footer.php");
