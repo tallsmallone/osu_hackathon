62f619b63bfff8895f4b3ee6df73c5c44c98031f
Nick Flint <nick.r.flint@gmail.com>
2015-11-14 15:53:08 -0500

    Added large container

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	require_once("frames/head.php"); 
 ?>
-	<div>
+	<div style="width:100%; height=500px">
 		<div class="input-group input-group-lg"  style="top:250px; left:25%; width:50%;">
 	   		<input type="text" class="form-control" placeholder="Where to Eat" aria-describedby="sizing-addon1">
 	   	</div>
