b3f5d3da664fb1862051e774e907c5029c29b843
Nick Flint <nick.r.flint@gmail.com>
2015-11-15 05:51:20 -0500

    changed title

--- frames/head.php
+++ frames/head.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>osuDining</title>
+    <title>eduDining</title>
 
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-theme.css" rel="stylesheet">
